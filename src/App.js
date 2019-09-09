666e33b44ca9b2921467a4c4bf32d909e3b5fbfb
tatsuya <osakana3ex.hahaha@gmail.com>
2019-09-09 23:15:02 +0900

    コンポーネント作成

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Hoge from "./component/hoge";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>こんにちは</h1>
+        <Hoge />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
