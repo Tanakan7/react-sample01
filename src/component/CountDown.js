f7e6625dff25859ae40f61e6d43ad29dc6a4d95b
tatsuya <osakana3ex.hahaha@gmail.com>
2019-09-14 16:56:00 +0900

    ポーズ機能

--- src/component/CountDown.js
+++ src/component/CountDown.js
@@ -10,11 +10,13 @@ class CountDown extends React.Component {
       timerValue: '',
       str: 'サンプル',
     }
+
+    this._stopCountDown = this._stopCountDown.bind(this)
   }
 
   componentDidMount() {
     console.log('componentDidMount CountDonw');
-    this.timer1.start({ countdown: true, startValues: { seconds: 4 } });
+    this.timer1.start({ countdown: true, startValues: { seconds: 10 } });
     this.timer1.addEventListener('secondsUpdated', () => {
       this.setState({
         timerValue: this.timer1.getTimeValues().toString()
@@ -25,10 +27,22 @@ class CountDown extends React.Component {
       console.log('タイマー終了')
     })
   }
+
+  _stopCountDown() {
+    // this._stopCountDown = this._stopCountDown.bind(this) コンストラクタでこれやってないとエラー
+    // this.なんとか のなかで this.ほげほげ を呼び出すときは必要っぽい？
+    if (this.timer1.isRunning()) {
+      this.timer1.pause();
+    } else {
+      this.timer1.start();
+    }
+  }
+
   render() {
     return (
       <div className='wrap'>
         <p>{this.state.timerValue}</p>
+        <button onClick={this._stopCountDown}>ストップ</button>
       </div>
     )
   }
